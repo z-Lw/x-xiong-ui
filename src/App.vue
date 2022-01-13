f11ca14ba51ac977ac052d4139bf4e4bcf43ea63
z-Lw <553870855@qq.com>
2022-01-13 17:09:26 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    hi
+    <Wei/>
+  </div>
+</template>
+
+<script>
+import Wei from './components/Wei.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Wei
+  }
+}
+</script>

52612b77a1d202ccedebc5256f02d03ff512b94e
z-Lw <553870855@qq.com>
2022-04-20 22:34:14 +0800

    优化css

--- src/lib/switch.vue
+++ src/lib/switch.vue
@@ -32,28 +32,36 @@ export default {
         height: $h;
         width: $h*2;
         border: none;
-        background: grey;
+        background: #bfbfbf;
         border-radius: $h/2;
         position: relative;
         cursor: pointer;
+        > span{
+            position: absolute;
+            top: 2px;
+            left: 2px;
+            height: $h2;
+            width: $h2;
+            background:white;
+            border-radius: $h2 / 2;
+            transition: all 250ms;
+        }
+        &.checked{
+            background: blue;
+            >span{
+                left: calc(100% - #{$h2} - 2px)
+            }
+        }
+        &:focus{
+            outline: none;
+        }
+        &:active{
+            > span {width: $h2 + 4px;}
+        }
+        &.checked:active{
+            > span {width: $h2 + 4px; margin-left: -4px;}
+        }
     }
-    span{
-        position: absolute;
-        top: 2px;
-        left: 2px;
-        height: $h2;
-        width: $h2;
-        background:white;
-        border-radius: $h2 / 2;
-        transition: left 250ms;
-    }
-    button.checked{
-        background: blue;
-    }
-    button.checked >span{
-        left: calc(100% - #{$h2} - 2px)
-    }
-    button:focus{
-        outline: none;
-    }
+
+
 </style>
--- src/view/Doc.vue
+++ src/view/Doc.vue
@@ -65,7 +65,7 @@
         > main {
             flex-grow: 1;
             padding: 16px;
-            background: lightgreen;
+            background: white;
         }
     }
     aside{

04bb30e6045b9ae85a898202c648f1545167b9d1
z-Lw <553870855@qq.com>
2022-04-21 16:26:58 +0800

    css最小影响

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-    <button  @click="toggle" :class="{checked: value}">
+    <button  class="gulu-switch" @click="toggle" :class="{'gulu-checked': value}">
         <span></span>
     </button>
     <div>
@@ -25,10 +25,10 @@ export default {
 
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
     $h: 22px;
     $h2: $h - 4px;
-    button{
+    gulu-switch{
         height: $h;
         width: $h*2;
         border: none;
@@ -46,7 +46,7 @@ export default {
             border-radius: $h2 / 2;
             transition: all 250ms;
         }
-        &.checked{
+        &.gulu-checked{
             background: blue;
             >span{
                 left: calc(100% - #{$h2} - 2px)
@@ -58,7 +58,7 @@ export default {
         &:active{
             > span {width: $h2 + 4px;}
         }
-        &.checked:active{
+        &.gulu-checked:active{
             > span {width: $h2 + 4px; margin-left: -4px;}
         }
     }

136246d086975a738e6ea6b799854b9a5ff0ee71
z-Lw <553870855@qq.com>
2022-04-21 16:04:35 +0800

    添加theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,14 +1,22 @@
 <template>
-    <button>
-        <slot/>
-    </button>
-
+        <button class="gulu-button" :class="`theme-${theme}`">
+            <slot/>
+        </button>
 </template>
 
 <script lang="ts">
-
+export default {
+   props:{
+     theme:{
+       type:String,
+       default:'button'
+     },
+   }
+}
 </script>
 
 <style lang="scss" scoped>
-
+    div{
+        border:1px solid red;
+    }
 </style>

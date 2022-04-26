9ed0af28c8c2ca69f0d4482de10a1eb3a8888446
z-Lw <553870855@qq.com>
2022-04-26 14:10:33 +0800

    基本完成Button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,4 +1,5 @@
 <template>
+    <div>
     <div>Button 示例</div>
     <h1>示例1</h1>
     <Button > 你好</Button>
@@ -37,7 +38,16 @@
             <Button theme="text">普通文字按钮</Button>
             <Button theme="text" level="danger">危险文字按钮</Button>
         </div>
-
+        <h1>示例4</h1>
+        <div>
+            <Button disabled> 禁用按钮</Button>
+            <Button :disabled="true"> 禁用按钮</Button>
+            <Button theme="text" disabled> 禁用按钮</Button>
+            <Button theme="link" disabled> 禁用链接按钮</Button>
+        </div>
+        <h1>示例5</h1>
+        <Button loading>加载中</Button>
+        <Button >加载完成</Button>
     </div>
 
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
-    <button class="gulu-button" :class="classes">
+    <button class="gulu-button" :class="classes" :disabled="disabled">
+        <span v-if="loading" class="gulu-loadingIndicator"></span>
         <slot />
     </button>
 </template>
@@ -19,6 +20,14 @@ export default {
      level:{
        type:String,
        default:"normal",
+     },
+     disabled:{
+       type:Boolean,
+       default:false
+     },
+     loading:{
+       type:Boolean,
+       default:false
      }
    },
   setup(props){
@@ -42,6 +51,7 @@ export default {
     $blue: #40a9ff;
     $radius: 4px;
     $red: red;
+    $grey: grey;
     .gulu-button {
         box-sizing: border-box;
         height: $h;
@@ -146,6 +156,37 @@ export default {
                 }
             }
         }
-
+        &.gulu-theme-button {
+            &[disabled] {
+                cursor: not-allowed;
+                color: $grey;
+                &:hover {
+                    border-color: $grey;
+                }
+            }
+        }
+        &.gulu-theme-link, &.gulu-theme-text {
+            &[disabled] {
+                cursor: not-allowed;
+                color: $grey;
+            }
+        }
+        > .gulu-loadingIndicator{
+            width: 14px;
+            height: 14px;
+            display: inline-block;
+            margin-right: 4px;
+            border-radius: 8px;
+            border-color: $blue $blue $blue transparent;
+            border-style: solid;
+            border-width: 2px;
+            animation: gulu-spin 1s infinite linear;
+        }
     }
+    @keyframes gulu-spin {
+        0%{transform: rotate(0deg)}
+        100%{transform: rotate(360deg)}
+    }
+
+
 </style>
